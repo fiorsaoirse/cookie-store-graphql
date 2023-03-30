886d14fb5630f843055aeb698d3cf25c6f5f8dfe
DMurdasova <solovyanovamaria@yandex.ru>
2023-03-30 20:24:03 +0200

    init GraphQL

---
+++ src/index.ts
@@ -0,0 +1,214 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+interface ITopping {
+    id: number;
+    name: string;
+}
+
+interface ICookie {
+    id: number;
+    title: string;
+    description?: string;
+    toppingIds: number[];
+    price: number;
+    rating: number;
+}
+
+interface ICookieWithToppings extends ICookie {
+    toppings: ITopping[];
+}
+
+type SortOrder = 'none' | 'price' | 'rating';
+type SortType = 'asc' | 'desc';
+
+interface IFilter {
+    term?: string;
+    selectedToppings: number[];
+    sortType: SortType;
+    sortOrder: SortOrder;
+    priceRange?: {
+        from?: number;
+        to?: number;
+    };
+}
+
+const typeDefs = `#graphql
+  type Topping {
+    id: ID!
+    name: String
+  }
+
+  type Cookie {
+    id: ID!
+    title: String!
+    description: String
+    toppingIds: [ID!]
+    toppings: [Topping!]
+    price: Int!
+    rating: Float
+  }
+
+  input PriceRange {
+    from: Int
+    to: Int
+  }
+
+  enum SortType {
+    asc
+    desc
+  }
+
+  enum SortOrder {
+    none
+    price
+    rating
+  }
+
+  input Filter {
+    term: String
+    selectedToppings: [Int]
+    sortType: SortType
+    sortOrder: SortOrder
+    priceRange: PriceRange
+  }
+
+  type Query {
+    toppings: [Topping]
+    cookies(filter: Filter!): [Cookie]
+  }
+`;
+
+const toppings: ITopping[] = [
+    {
+        "id": 1,
+        "name": "Chocolate"
+    },
+    {
+        "id": 2,
+        "name": "Strawberry"
+    },
+    {
+        "id": 3,
+        "name": "Coconut"
+    },
+    {
+        "id": 4,
+        "name": "Marshmallows"
+    },
+    {
+        "id": 5,
+        "name": "Raspberry"
+    }
+];
+
+const cookies: ICookie[] = [
+    {
+        "id": 1,
+        "title": "Chocolate cookie",
+        "description": "Wonderful crispy cookie!",
+        "toppingIds": [1],
+        "price": 100,
+        "rating": 4.5
+    },
+    {
+        "id": 2,
+        "title": "Nuts cookie",
+        "toppingIds": [],
+        "price": 120,
+        "rating": 5
+    },
+    {
+        "id": 3,
+        "title": "Berries cookie",
+        "toppingIds": [2, 5],
+        "price": 150,
+        "rating": 5
+    },
+    {
+        "id": 4,
+        "title": "Coconut cookie",
+        "description": "Tasty and sweet",
+        "toppingIds": [3],
+        "price": 100,
+        "rating": 4.8
+    },
+    {
+        "id": 5,
+        "title": "Chocolate-marshmallow",
+        "description": "Incredibly tasty!",
+        "toppingIds": [1, 4],
+        "price": 160,
+        "rating": 3.6
+    },
+    {
+        "id": 6,
+        "title": "MM's cookie",
+        "toppingIds": [],
+        "price": 90,
+        "rating": 4.3
+    }
+];
+
+const compareFunction = (desc: boolean) => (aProp: number, bProp: number) => {
+    return desc ? (bProp - aProp) : (aProp - bProp);
+};
+
+const resolvers = {
+    Query: {
+        toppings: (): ITopping[] => toppings,
+        cookies: (_, { filter }: { filter: IFilter; }): ICookieWithToppings[] => {
+            const {
+                term, sortOrder,
+                sortType, selectedToppings,
+                priceRange
+            } = filter;
+
+            let result = [...cookies];
+
+            if (term) {
+                const r = new RegExp(term, 'ig');
+                result = result.filter((cookie: ICookie) => r.test(cookie.title));
+            }
+
+            if (selectedToppings?.length) {
+                result = result.filter((cookie: ICookie) => {
+                    return cookie.toppingIds?.some((tID: number) => selectedToppings.includes(tID)) ?? false;
+                });
+            }
+
+            if (priceRange) {
+                result = result.filter((cookie: ICookie) => {
+                    const lowerBound = cookie.price >= priceRange.from ?? 0;
+                    const topBound = lowerBound && (cookie.price <= priceRange.to ?? Infinity);
+
+                    return lowerBound && topBound;
+                });
+            }
+
+            if (sortOrder !== 'none') {
+                const comparator = compareFunction(sortType === 'desc');
+
+                result = result.sort((a: ICookie, b: ICookie) => {
+                    return comparator(a[sortOrder], b[sortOrder]);
+                });
+            }
+
+            return result.map((cookie: ICookie): ICookieWithToppings => {
+                const t = toppings.filter((t: ITopping) => cookie.toppingIds.includes(t.id));
+                return { ...cookie, toppings: t };
+            });
+        }
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
